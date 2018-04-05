28319a25eed5492cdb2fe59e8557ac37d8f25e0d
mint <parichat@techno-chon.ac.th>
2018-04-05 18:41:08 +0700

    mint

--- home.php
+++ home.php
@@ -6,8 +6,8 @@
 			 <div id="myCarousel" class="carousel slide" data-ride="carousel">
    
 				<ol class="carousel-indicators" style="bottom:0px;" id="showSlideNews_number">
-				  <li data-target="#myCarousel" data-slide-to="0" ></li>
-				   <!---<li data-target="#myCarousel" data-slide-to="1"></li>
+				 <!--- <li data-target="#myCarousel" data-slide-to="0" ></li>
+				   <li data-target="#myCarousel" data-slide-to="1"></li>
 				  <li data-target="#myCarousel" data-slide-to="2"></li>
 				  <li data-target="#myCarousel" data-slide-to="3"></li> --->
 				</ol>
