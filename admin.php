0be077e4e07c544c9c781cb5e77acea1e4077cc2
mint <parichat@techno-chon.ac.th>
2018-05-01 09:53:21 +0700

    mint  -อัพเดทหลังพี่จอยปรับหน้าแอดมิน

--- admin.php
+++ admin.php
@@ -1,110 +1,30 @@
-<!--
-<body class="bgHome container">
-
-	<div class="row adminPage">
-	
-			<div class="marginAuto" >
-				  <p class="textName"></p>
-				  <div class="form-group paddingT10 marginAuto divBtn">
-					<center>
-					<button type="button" class="btn btn-warning adminBtn marginT20" id="approveBtn">Approve Story</button><br/>
-					<button type="button" class="btn btn-warning adminBtn" id="createStoryBtn">Create Story</button><br/>
-					<button type="button" class="btn btn-warning adminBtn" id="manageStoryBtn">Manage Story</button><br/>
-					<button type="button" class="btn btn-warning adminBtn" id="manageCateBtn">Manage Category</button><br/>
-					<button type="button" class="btn btn-warning adminBtn" id="editNewsBtn">Edit News</button>
-					
-					</center>
-					<div class="btn btn-warning btn-circle" id="createBtn" ><i class="fa fa-plus"></i></div>
-				  </div>
-			</div>	
-	</div>
-    <script>
-		$(document).ready(function(){
-			//สีฟ้าที่ปุ่ม admin
-			$(".adminPage").addClass('active')
-			$(".storyPage").hide()
-			
-			$(".textName").text("Joy!")
-			
-			
-			$('#approveBtn').click(function() {
-				
-				alert("Approve Story")
-			});
-			
-			$('#createStoryBtn').click(function() {
-				alert("Create Story")
-				
-			});
-			
-			$('#manageStoryBtn').click(function() {
-				alert("Manage Story")
-				
-			});
-			
-			$('#manageCateBtn').click(function() {
-				alert("Manage Category")
-				window.location.href="main.php?page=admin_manageCategory";
-				
-			});
-			
-			$('#editNewsBtn').click(function() {
-				alert("Edit News")
-				window.location.href="main.php?page=admin_manageNews";
-				
-				
-			});
-			
-			$('#createBtn').click(function() {
-				alert("create")
-				
-			});
-			
-			
-			
-			
-			
-			  
-			$('ul li').click(function() {
-				$('ul.nav-tabs li.active').removeClass('active');
-				$(this).closest('li').addClass('active');
-			});
-			
-			
-		});
-			
-	</script>
-
-</body>
-</html>-->
-
 
 <div class="row adminPage">
 	<div class="marginAuto divBtn min-h">
 		<div class="row marginAuto"  style="width:95%;">
 			<div class="imgAdmin marginAuto marginB10" ></div>
 			<div class="blockAdmin marginAuto">
-				<div class="blockAdminInner" >
+				<div class="blockAdminInner marginT20" >
 					<div class="blockAdminBtn" id="approveBtn">
-						<div class="icon40 icon-check marginAuto"></div>
+						<div class="icon48 icon-check marginAuto"></div>
 						<p class="text-center"><b>Approve Story</b></p>
 					</div>
 				</div>
-				<div class="blockAdminInner">
+				<div class="blockAdminInner marginT20">
 					<div class="blockAdminBtn"  id="manageStoryBtn">
-						<div class="icon40 icon-book marginAuto"></div>
+						<div class="icon48 icon-book marginAuto"></div>
 						<p class="text-center"><b >Manage Story</b></p>
 					</div>
 				</div>
 				<div class="blockAdminInner">
 					<div class="blockAdminBtn" id="manageNewsBtn">
-						<div class="icon40 icon-news2 marginAuto"></div>
+						<div class="icon48 icon-news2 marginAuto"></div>
 						<p class="text-center"><b >Manage News</b></p>
 					</div>
 				</div>
 				<div class="blockAdminInner">
 					<div class="blockAdminBtn" id="manageCateBtn">
-						<div class="icon40 icon-folder marginAuto"></div>
+						<div class="icon48 icon-folder marginAuto"></div>
 						<p class="text-center"><b >Manage Category</b></p>
 					</div>
 				</div>
@@ -121,7 +41,7 @@
 		//สีฟ้าที่ปุ่ม admin
 		$(".adminPage").addClass('active')
 		$(".storyPage").hide()
-		$(".mainDetail").css('background-color', '#dddddd');
+		$(".mainDetail").css('background-color', '#FFF');
 		
 		$('#approveBtn').click(function() {
 				alert("Approve Story : ======> ทำต่อด้วย")
--- main.php
+++ main.php
@@ -8,7 +8,7 @@ $_GET['storyID'];
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 
-	<!--<link rel="stylesheet" href="css/bootstrap.min.css">-->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<script src="js/jquery.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<!-- สริปต์ปุ่มสีๆ -->
