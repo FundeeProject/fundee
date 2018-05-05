517cfd48d0ff57aa18472fbba86eb4f418fc26c6
mint <parichat@techno-chon.ac.th>
2018-05-05 18:58:01 +0700

    mint

---
+++ create_page_detail.php
@@ -0,0 +1,157 @@
+<?php
+session_start();
+$_GET['storyid'];
+$_GET['pageid'];
+?>
+<br>
+
+<div id = "showCreateTitle" class = "showCreateTitle" style=""> 
+	<form class="marginT20" style="width :90%; margin-left:auto; margin-right:auto; "  method = "post" enctype = "multipart/form-data" id = "createform2">
+		<div class="form-group">
+			<label for="">Story Name : <span id="showStoryname2"></span></label>
+		</div>
+		<div class="form-group">
+			<label for="">Please add picture, Record and text. </label>
+			<div class="box-img" style="width:90%" >
+				<div class="box-text-number">
+					<p class="text-center">1</p>
+				</div>
+				<div class="objectCenter new_Btn2 "  id = "showPic_page">
+					<!--<i class="fa fa-camera" style="font-size:24px"></i>--> Add Pic
+				</div>
+				
+				<input type="file" name="pic_upload_page" id="html_btn2" OnChange="PreviewPage(this)"/>
+				
+				<p class="text-center objectCenter" style="top:55%">Add Picture</p>
+			</div>
+			
+			
+		</div>
+		
+		<div class="row marginB20" style="margin-top: -10px;">
+			<div class="col-xs-3 col-sm-3">
+				<div id="" class="form-group form-group-sm">
+					<div class="bg-btn btn-create " style="margin-left:40px;" id="editText">
+						<i class="icon-text " >editText</i> 
+					</div>
+					<input type="hidden" name="editText"  />
+					<!--<input type="text" name="editText" id="editText" />-->
+				</div>
+			</div>
+			<div class="col-xs-3 col-sm-3">
+				<div id="" class="form-group form-group-sm">
+					<div class="bg-btn btn-refresh" style="margin-left:20px;">
+						<i class="fa fa-refresh" ></i>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-3 col-sm-3">
+				<div id="" class="form-group form-group-sm">
+					<div class="bg-btn btn-microphone" id="editSound">
+						 <i class="icon-mic" style="">Audio</i> 
+					</div>
+					<input style="" type="file" name="audio_upload" id="audio_upload" accept="audio/*" />
+				</div>
+			</div>
+			<div class="col-xs-3 col-sm-3">
+				<div id="" class="form-group form-group-sm">
+					<div class="bg-btn btn-play" style="margin-left:-20px;">
+						<i class="fa fa-play-circle" ></i>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		<div style="display:none;" id = "show_description_page">
+			<label for="description_page" class="marginT10 color-blue">เนื้อเรื่อง</label>
+			<textarea class="form-control marginB20" id="description_page" rows="3">  </textarea>
+		</div>
+		<div class="btn-confirm btn-ok">
+			<button type="submit" id = "SavePage" class="btn btn-primary wp100">SavePage</button>
+		</div>							  
+	</form>
+</div>
+<script>
+	$(document).ready(function(){
+		$(".storyPage").addClass('active');
+		$(".adminPage").hide();
+		var story_id = <?php echo $_GET['storyid'];?> ;
+		var  pageNumber_ = <?php echo $_GET['pageid'];?>;
+		$('.new_Btn2').bind("click" , function () {
+			$('#html_btn2').click();
+		});
+		
+		$("#editText").click(function(){ /// กดเพิ่มเนื้อเรื่องนิทาน
+			var show_description_page = document.getElementById("show_description_page");
+			show_description_page.style.display = "block";
+		});
+		//------------------------------------------->> แสดงชื่อเรื่อง <<---------------------------------------->
+		$.ajax({
+			type:'POST',
+			url:'qs/qs_findbookby_id.php',
+			dataType: "json",
+			data: {storyid:story_id},
+			success:function( datajson ) {  
+				if(datajson.length !=0){ 
+					var showStoryname2 = document.getElementById("showStoryname2");
+					showStoryname2.innerHTML = ""+datajson[0].story_name+"" ;
+				}
+				else{
+					  alert("ไม่พบข้อมูลหน้านิทาน");
+				}
+			},
+			error:function(jqXHR, textStatus, errorThrown){alert(errorThrown);}		
+		});
+		//------------------------------------------->> แก้ไขหน้าของนิทานเมื่อด Save Page <<---------------------------------------->
+		$("#createform2").on("submit",function(e){ 
+			e.preventDefault();
+			//----------------ข้อมูลที่จะส่งไปอัพเดท
+			var  description_page = document.getElementById("description_page");
+			var formData2 = new FormData($(this)[0]);
+			formData2.append("story_id",story_id);
+			formData2.append("pageNumber",pageNumber_);
+			formData2.append("description_page",description_page.value);
+			//-------------------------
+			var str = $('#img2').attr('src');
+			if (typeof str === "undefined" || $("#description_page").val() == ""  ) {
+				alert("กรุณากรอกข้อมูลให้ครบ");
+			}
+			else{
+				//alert("ครบ"+pageNumber_);
+				///////อัพเดทลงฐานข้อมูล
+				$.ajax({
+		            url: 'qs/qs_uploadPic_to_page.php',
+		            type: 'POST',
+				    data: formData2,
+		            async: false,
+		            cache: false,
+		            contentType: false,
+		            processData: false
+		        }).done(function(data){
+		              //  alert("-"+data+"-");
+					if(data == "notsuccess"){
+						alert("เพิ่มไม่ได้ กรุณาลองใหม่");
+					}
+					else if(data == "ok"){
+						alert("เพิ่มแล้ว");
+						window.location.href="main.php?page=create_page&storyid="+story_id+"";
+					}
+		        });
+			}
+		});
+	});
+	function PreviewPage(ele) {  // แสดงรูปที่เลือก เพจ
+		var showPic_page = document.getElementById("showPic_page");
+		showPic_page.innerHTML = "<img id='img2' src='' alt='' style=' width: 100%; height: 120px;' />";
+		$('#img2').attr('src', ele.value);
+		if (ele.files && ele.files[0]) 
+		{
+			var reader = new FileReader();
+			reader.onload = function (e) 
+			{
+				$('#img2').attr('src', e.target.result);
+			}
+			reader.readAsDataURL(ele.files[0]);
+		}
+	}
+</script>
---
+++ main_bac.php
@@ -0,0 +1,284 @@
+<?php
+session_start();
+$_GET['page'];
+$_GET['storyID'];
+?>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<!-- สริปต์ปุ่มสีๆ -->
+	<!--<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">-->
+	<!-- Add icon library -->
+	<!--<link rel="stylesheet" href="css/fontawesome-all.min.css">-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.navbarH {
+  overflow: hidden!important; 
+  background-color:#085b6b!important; 
+  position: fixed!important; 
+  top: 0!important; 
+  width: 100%!important; 
+  height:188px!important; 
+  border:0px!important; 
+	left:0px;
+  z-index: 99;
+}
+
+
+.navbarHK{
+	position:relative!important; 
+
+}
+
+
+.nav-tabs{border:0}
+
+.mainH {
+  background-color:#085b6b!important; 
+  position: absolute;
+  top: 188!important; 
+  width: 100%!important; 
+  /*height:260px!important;*/ 
+  border:0px!important; 
+  left:0px;
+  z-index: 0;
+  overflow-x: hidden;
+}
+
+.mainBg{  
+	width: 95%!important;    
+	margin-left: auto!important;
+	margin-right: auto!important;
+}
+.mainDetail{
+	background-color: #FFF;
+	padding:0 16px!important;
+	border-bottom-left-radius: 10px!important;
+    border-bottom-right-radius: 10px!important;
+}
+
+.bgW_login{
+	padding-top: 37px!important;
+}
+
+/*modal*/
+.modal{
+	top: 40%;
+    width: 90%;
+    left: 0;
+    right: 0;
+    margin: 0 auto;"
+}
+.modal-footer{
+	border:0!important;
+	width: 220px;
+    position: absolute;
+    bottom: 0px;
+    margin: 0 auto;
+    left: 0;
+    right: 0;	
+}
+
+</style>
+</head>
+<body class="bgHome container">
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '1948365968761538',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v2.11'
+    });
+	
+	FB.getLoginStatus(function(response) {
+     // statusChangeCallback(response);
+		if(response.status === 'connected'){
+			//document.getElementById('status').innerHTML ='We are connected';
+			//document.getElementById('login').style
+		}
+		 else if(response.status === 'not_authorized'){
+			//document.getElementById('status').innerHTML ='We are not login';
+		}
+		else{
+			//document.getElementById('status').innerHTML ='you are not logged into faceboook ';
+		}
+    });
+	
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+   function login(){
+		FB.login(function(response){
+			if(response.status === 'connected'){
+				document.getElementById('status').innerHTML ='We are connected';
+				window.location.href='index2.php'; /// เปิดไปหน้าใหม่
+				//return false;
+			}
+			 else if(response.status === 'not_authorized'){
+				document.getElementById('status').innerHTML ='We are not login';
+			}
+			else{
+				document.getElementById('status').innerHTML ='you are not logged into faceboook ';
+			}
+		});
+   }
+   function info(){
+		FB.api(	'/me',
+				'GET',
+				{fileds:'first_name,last_name_name_id'},
+				function(response){
+					document.getElementById('status').innerHTML=response.name;
+				}
+				);
+   }
+$(document).ready(function(){
+	
+});
+	
+	
+</script>
+
+
+<div class="navbarH">
+	
+
+<div style="position:absolute"><?php echo $_SESSION["role_id"];?> </div>
+
+   <div class="masterProfile">
+	   <i class="fa fa-sign-out-alt"></i>
+	   <span class="glyphicon glyphicon-log-out">
+   </div>
+   <div class="nameLogo" style="width: 130px; height: 53px;"></div>
+   <div class="w100 logoHome login marginT10" style="position:retative; top:-20px;"></div>
+
+	<div class="row ">
+		<div class="col-xs-12 col-lg-offset-3 col-lg-6">
+			<div class="bgW_login contentStory marginB10 border-bottom" style="">
+				<section class="area">
+					 <div class="row">
+						<div class="col-md-12" style="padding:0px;">
+							<ul class="nav nav-tabs list-group" style="margin-bottom:0;">
+								<li class="homePage"><a href="main.php?page=home">HOME</a></li>
+								<li class="categoryPage"><a href="main.php?page=category">CATEGORY</a></li>
+								<li class="storyPage"><a href="main.php?page=mystory">MY STORY </a></li>
+								<?php if($_SESSION["role_id"] == 0){echo '<li class="adminPage"><a href="main.php?page=admin">ADMIN </a></li>';} ?>
+							</ul>
+						</div>
+					 </div>
+				</section>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+<div class="mainH">
+	<div class="row marginB10">
+		<div class="col-xs-12 col-lg-offset-3 col-lg-6">
+			<div class="mainBg" >
+				<div class="mainDetail" >
+				
+						<?php 
+							if($_GET['page'] == 'home'){
+								include("home.php");
+							}else if($_GET['page'] == 'category'){
+								include("category.php");
+							}else if($_GET['page'] == 'mystory'){
+								include("mystory.php");
+							}else if($_GET['page'] == 'admin'){
+								include("admin.php");
+							}else if($_GET['page'] == 'admin_addNews'){
+								include("admin_addNews.php");
+							}else if($_GET['page'] == 'admin_manageNews'){
+								include("admin_manageNews.php");
+							}else if($_GET['page'] == 'admin_manageCategory'){
+								include("admin_manageCategory.php");
+							}else if($_GET['page'] == 'admin_addCategory'){
+								include("admin_addCategory.php");
+							}else if($_GET['page'] == 'admin_approveStory'){
+								include("admin_approveStory.php");
+							}else if($_GET['page'] == 'admin_manageStory'){
+								include("admin_manageStory.php");
+							}else if($_GET['page'] == 'create_story'){
+								include("create_story.php");
+							}else if($_GET['page'] == 'admin_approveStoryDetail'){
+								include("admin_approveStoryDetail.php");
+								
+							}else if($_GET['page'] == 'admin_manageStoryDetail'){
+								include("admin_manageStoryDetail.php");
+							}else if($_GET['page'] == 'full_page'){
+								include("full_page.php");
+							}else if($_GET['page'] == 'play_story'){
+								if($_GET['storyID'] != ""){
+									include("play_story.php");
+								}
+							}else if($_GET['page'] == 'play_storydetail'){
+								if($_GET['storyID'] != ""){
+									include("play_storydetail.php");
+								}
+							}else if($_GET['page'] == 'play_storydetail_formystory'){
+								if($_GET['storyID'] != ""){
+									include("play_storydetail_formystory.php");
+								}
+							}else if($_GET['page'] == 'create_page'){
+								if($_GET['storyid'] != ""){
+									include("create_page.php");
+								}
+							}
+							
+							
+						?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+  <div class="modal-dialog" role="document">
+    <div class="modal-content" style="min-height: 150px;">
+      <div class="modal-body" style="text-align:center;">
+      </div>
+      <div class="modal-footer marginAuto" style="" >
+        <button type="button" class="btn btn-secondary noBtn" id="noModalBtn" style="width:90px;" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-info okBtn" id="okModalBtn" style="width:90px;">Yes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<script type="text/javascript">
+$(document).ready(function(){
+//=======================>>>>> logout <<<<<=======================
+	$(".glyphicon-log-out").click(function(){ 
+		
+		//alert("logout : ====== >4. ทำต่อด้วย!!!!!!!!!!!!!!!!")	
+		window.location.href="logout.php";
+	});	
+});
+</script>
+</body>
+</html>
